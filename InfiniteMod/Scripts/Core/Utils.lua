e3f40c27dbe53c307f8dca4124a69cafc891875f
raidrushgaming-star <raidrushgaming@gmail.com>
2026-07-13 05:28:22 +0200

    Integrate Timing.lua functions and constants into Utils.lua

--- InfiniteMod/Scripts/Core/Utils.lua
+++ InfiniteMod/Scripts/Core/Utils.lua
@@ -317,4 +317,48 @@ function Utils.runOnGameThread(fn)
     end
 end
 
+-- Timing module functions
+local tonumber = Utils.tonumber
+
+function Utils.CreateChecker(intervalMs)
+    local lastTime = 0
+    local interval = (tonumber(intervalMs) or 0) / 1000
+    return function()
+        local now = os_clock()
+        if now - lastTime < interval then
+            return false
+        end
+        lastTime = now
+        return true
+    end
+end
+
+function Utils.CreateKeyedChecker(intervalMs)
+    local lastTimes = {}
+    local interval = (tonumber(intervalMs) or 0) / 1000
+    return function(key)
+        local now = os_clock()
+        local k = Utils.tostring(key)
+        if lastTimes[k] and now - lastTimes[k] < interval then
+            return false
+        end
+        lastTimes[k] = now
+        return true
+    end
+end
+
+-- Timing constants
+Utils.NOTIFY_DEBOUNCE = 100
+Utils.KEY_DEBOUNCE = 200
+Utils.HOOK_DEBOUNCE = 200
+Utils.HOOK_SLOW = 1000
+Utils.HOOK_VERY_SLOW = 1500
+Utils.DELAY_SHORT = 500
+Utils.DELAY_MEDIUM = 1500
+Utils.DELAY_STANDARD = 2000
+Utils.DELAY_LONG = 3000
+Utils.CACHE_CLEANUP_INTERVAL = 300000
+Utils.MAX_FINDALL_SMALL = 500
+Utils.MAX_FINDALL_STANDARD = 2000
+
 return Utils
